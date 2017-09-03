57360d7baf6b9f84293de673db5045229a87d5d0
Patrick Pan <DPL@Donalds-MacBook-Air.local>
2017-09-03 00:03:36 -0600

    added a bit of fluff to user models

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,18 @@ Rails.application.routes.draw do
   root 'users#index'
   devise_for :users
 
-  resources :users
+
+
+  resources :users do 
+    resources :carts do
+      
+  end
+
+  resources :products
+  get 'edit_user_cart_path', to: 'carts#edit', as: 'update_cart'
+end
+
+
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
