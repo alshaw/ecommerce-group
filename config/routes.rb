b6dfecf9536ffc1eda1b7357d4ef81620203259b
Allison Shaw <shaw.allison91@gmail.com>
2017-09-04 17:48:59 -0600

    catching the branches up

--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,10 @@ Rails.application.routes.draw do
   end
 
   resources :products
-  get 'edit_user_cart_path', to: 'carts#edit', as: 'update_cart'
+  get 'edit_user_cart_path', to: 'carts#edit', as: 'update_cart' 
 end
 
-
+get '/products/:category', to: 'products#category'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
