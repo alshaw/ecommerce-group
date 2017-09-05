48ebf0687c049d4067ae1b22efec95b66bb35d94
Allison Shaw <shaw.allison91@gmail.com>
2017-09-04 21:58:55 -0600

    working on a cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,3 +2,15 @@ class Cart < ApplicationRecord
   belongs_to :user
   has_many :products, optional: true
 end
+
+	def count
+		products.count
+	end
+
+	def total
+		prices_array = products.map {|x| x.price.to_i}
+		total = prices_array.inject {|sum,n| sum + n}
+
+		# return prices
+		# products.inject {|total, product| total.price.to_i + product.price.to_i}
+	end
